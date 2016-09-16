a9e40d8376ca9f185703a80ba74d3377f003d408
Jonathan Andersson <ja1984@gmail.com>
2016-09-16 11:24:12 +0200

    [FIX] Added feature toggle to france

--- src/Mvc/MvcTemplates/N2/Content/Edit.aspx.designer.cs
+++ src/Mvc/MvcTemplates/N2/Content/Edit.aspx.designer.cs
@@ -22,15 +22,6 @@ namespace N2.Edit {
         protected global::System.Web.UI.WebControls.LinkButton btnSavePublish;
         
         /// <summary>
-        /// btnPreviewMain control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnPreviewMain;
-        
-        /// <summary>
         /// btnPreview control.
         /// </summary>
         /// <remarks>
