48224c215087bfb10f8960c6598003b0092b2ce7
lpodolak <lukasz.podolak@gmail.com>
2018-02-22 18:09:15 +0100

    FA-1451 :factory: Remove AllowPartiallyTrustedCallersAttribute
    Having this attribute doesn't allow to compile the assmemblies that reference it not to have it. Let's remove it then.

--- src/Framework/N2/Properties/AssemblyInfo.cs
+++ src/Framework/N2/Properties/AssemblyInfo.cs
@@ -19,6 +19,5 @@ using System.Security;
 [assembly: Guid("485f74c4-6f2a-41b5-93cc-f5f64f84d037")]
 
 [assembly: CLSCompliant(true)]
-[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: InternalsVisibleTo("N2.Extensions.Tests")]
